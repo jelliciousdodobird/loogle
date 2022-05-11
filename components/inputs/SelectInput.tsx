eb949c321b28fb1e367fdc07276ca72114b12690
at235am <at235am@gmail.com>
2022-05-11 00:49:20 -0700

    added animations to the select input

--- components/inputs/SelectInput.tsx
+++ components/inputs/SelectInput.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 import { MdArrowDropDown } from "react-icons/md";
@@ -8,12 +9,16 @@ const Container = styled.div`
   position: relative;
 `;
 
-const Options = styled.ul`
+const Options = styled(motion.ul)`
+  z-index: 1;
+
   position: absolute;
   width: 100%;
+  margin-top: 0.5rem;
 
   border-radius: 5px;
   background-color: ${({ theme }) => theme.colors.surface.main};
+  background-color: ${({ theme }) => theme.colors.surface.dark};
 `;
 
 const Option = styled.li`
@@ -21,35 +26,50 @@ const Option = styled.li`
   color: white;
   border-radius: 5px;
   padding: 0.5rem;
+  user-select: none;
 
   &:hover {
-    background-color: ${({ theme }) => theme.colors.primary.main};
+    background-color: ${({ theme }) => theme.colors.background.darker};
+    font-weight: 600;
   }
 `;
 
 const SelectedOption = styled.div`
+  z-index: 2;
+  position: relative;
+
   cursor: pointer;
+  user-select: none;
 
   border-radius: 5px;
   padding: 0.5rem;
+  /* padding-right: 0; */
 
-  background-color: ${({ theme }) => theme.colors.primary.main};
+  background-color: ${({ theme }) => theme.colors.surface.dark};
 
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 0.5rem;
+  /* gap: 0.5rem; */
 `;
 
 const SelectedText = styled.p`
-  color: white;
+  /* color: white; */
+  /* border: 1px solid red; */
+
+  flex: 1;
+  font-weight: 600;
   white-space: nowrap;
 `;
 
-const ArrowIcon = styled.span`
+const ArrowIcon = styled(motion.span)`
   /* border: 1px solid red; */
   /* width: 2rem;
   height: 2rem; */
+
+  background-color: ${({ theme }) => theme.colors.surface.lighter};
+  border-radius: 3px;
+  border-radius: 50%;
 
   display: flex;
   justify-content: center;
@@ -73,30 +93,71 @@ type Props = {
 const SelectInput = ({ items, selected, handleChange, className }: Props) => {
   const [showOptions, setShowOptions] = useState(false);
 
+  const iconAnimProps = {
+    variants: {
+      initial: {
+        rotate: 0,
+      },
+      expand: {
+        rotate: 180,
+      },
+    },
+    initial: "initial",
+    animate: showOptions ? "expand" : "initial",
+
+    // transition: { duration: 0.5 },
+  };
+  const optionsAnimProps = {
+    variants: {
+      initial: {
+        y: -80,
+        opacity: 0,
+      },
+      expand: {
+        y: 0,
+        opacity: 1,
+      },
+    },
+    initial: "initial",
+    animate: showOptions ? "expand" : "initial",
+    exit: "initial",
+
+    // transition: { duration: 0.5 },
+  };
+
   const toggleShowOptions = () => setShowOptions((v) => !v);
 
   return (
     <Container className={className}>
       <SelectedOption onClick={toggleShowOptions}>
         <SelectedText>{selected}</SelectedText>
-        <ArrowIcon>
+        <ArrowIcon {...iconAnimProps}>
           <MdArrowDropDown />
         </ArrowIcon>
       </SelectedOption>
-      {showOptions && (
-        <Options role="listbox" aria-activedescendant={selected}>
-          {items.map((v) => (
-            <Option
-              key={v}
-              role="option"
-              aria-selected={selected === v}
-              onClick={() => handleChange(v)}
-            >
-              {v}
-            </Option>
-          ))}
-        </Options>
-      )}
+      <AnimatePresence>
+        {showOptions && (
+          <Options
+            {...optionsAnimProps}
+            role="listbox"
+            aria-activedescendant={selected}
+          >
+            {items.map((v) => (
+              <Option
+                key={v}
+                role="option"
+                aria-selected={selected === v}
+                onClick={() => {
+                  handleChange(v);
+                  setShowOptions(false);
+                }}
+              >
+                {v}
+              </Option>
+            ))}
+          </Options>
+        )}
+      </AnimatePresence>
     </Container>
   );
 };
